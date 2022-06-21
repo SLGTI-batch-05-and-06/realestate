dc8fb42f7a5a9f3b1ba644cc6c9ed28034789b6c
krishshadhu <krishadhu990510@gmail.com>
2022-06-21 15:04:55 +0530

    push

--- index.php
+++ index.php
@@ -1,13 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1> chiku01 lo,krggikltibhjukjt</h1>
+<?php include_once('script.php'); ?>
+<nav class="navbar navbar-expand-lg navbar-white  " style="background-color: #575555;" >
+ <img src="">
+    <a class="navbar-brand" href="#" style="color:white;">login</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#" style="color:white;">admin@gmail.com<span class="sr-only">(current)</span></a>
+            </li>
 
-</body>
-</html>
+
+        </ul>
+        <span class="navbar-text" style="color:white;">
+            call us:94770589968 &nbsp; &nbsp;
+        </span>
+        
+        
+
+    </div>
+</nav>
